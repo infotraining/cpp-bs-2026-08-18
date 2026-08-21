ef0edc4cd05bebd445bb3d1f8010fa0f587da64c
Krystian Piękoś <krystian.piekos@infotraining.pl>
2026-08-21 10:36:35 +0200

    Refactor BankAccount implementation; add constructors and print method; enhance test cases

--- oop/bank_account.hpp
+++ oop/bank_account.hpp
@@ -1,11 +1,33 @@
 #ifndef BANK_ACCOUNT_HPP
 #define BANK_ACCOUNT_HPP
 
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+
 namespace Banking
 {
-    class BankAccount
+    struct BankAccount
     {
-        // TODO
+        uint32_t id;
+        double balance = 0.0;
+
+        // constructor to initialize the account with an ID
+        BankAccount(uint32_t account_id) 
+        {
+            id = account_id;
+        }
+
+        BankAccount(uint32_t account_id, double account_balance)
+        {
+            id = account_id;
+            balance = account_balance;
+        }
+
+        void print() const
+        {
+            std::cout << "BankAccount(id: " << id << "; balance: " << balance << ")\n";
+        }
     };
 } // namespace Banking
 
--- oop/oop.cpp
+++ oop/oop.cpp
@@ -1,13 +1,45 @@
+#include "bank_account.hpp"
+
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
 #include <string>
 #include <vector>
 
-#include "bank_account.hpp"
-
 using namespace std::literals;
 
-TEST_CASE("oop")
+using namespace Banking;
+
+TEST_CASE("BankAccount - constructor")
 {
-	Banking::BankAccount account;
+    SECTION("only id")
+    {
+        BankAccount account(12345);
+
+        account.id = 12345;
+        CHECK(account.id == 12345);
+
+        account.print();
+    }
+
+	SECTION("id & balance")
+	{
+		BankAccount account(12345, 1'000);
+
+		CHECK(account.id == 12345);
+		CHECK(account.balance == 1'000);
+
+		account.print();
+	}
+}
+
+TEST_CASE("BankAccount - balance")
+{
+    Banking::BankAccount account(12345);
+
+    CHECK(account.balance == 0.0);
+
+    account.balance = 100.50;
+    CHECK(account.balance == 100.50);
+
+    account.print();
 }
