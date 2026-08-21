1b9a89be8f6684115f1ae71ac6afea5da791fa0d
Krystian Piękoś <krystian.piekos@infotraining.pl>
2026-08-21 14:29:03 +0200

    Refactor SFML demo; implement Circle class and update main function for improved graphics rendering

--- sfml-demo/src/main.cpp
+++ sfml-demo/src/main.cpp
@@ -1,21 +1,59 @@
 #include <SFML/Graphics.hpp>
 
+struct Point
+{
+    float x, y;
+
+    bool operator==(const Point& other) const
+    {
+        return x == other.x && y == other.y;
+    }
+};
+
+class Circle
+{
+    Point coord_;
+    double radius_;
+
+public:
+    Circle(Point coord, double r)
+        : coord_(coord)
+        , radius_(r)
+    { }
+
+    void move(float dx, float dy)
+    {
+        coord_.x += dx;
+        coord_.y += dy;
+    }
+
+    void draw(sf::RenderWindow& window)
+    {
+        sf::CircleShape circle(radius_);
+        circle.setPosition(sf::Vector2{coord_.x, coord_.y});
+        circle.setFillColor(sf::Color::Green);
+
+        window.draw(circle);
+    }
+};
+
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "SFML works!");
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
+    Circle c1(Point{200, 100}, 50);
 
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+		c1.move(1, 1);
+        c1.draw(window);
+        window.display();
+    }
 }
